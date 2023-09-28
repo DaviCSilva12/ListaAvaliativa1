9bcd5458458b1c919285b58e1385adb0668df99e
DaviCSilva12 <davicampanarosilva@gmail.com>
2023-09-28 19:11:54 -0300

    mudei

--- Questão4.c
+++ Questão4.c
@@ -6,8 +6,8 @@ int main() {
     float aumento; 
     float SalarioN;
 
-    scanf("%c", &nivel);
-    scanf("%f", &salarioA);
+        scanf("%c", &nivel);
+        scanf("%f", &salarioA);
 
         if(nivel == 'a'){
             aumento = salarioA * 0.05;
