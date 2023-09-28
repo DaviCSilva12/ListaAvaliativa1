96a655e4ce4cc942a9f53f89bdc1e180353ec4b2
DaviCSilva12 <davicampanarosilva@gmail.com>
2023-09-28 18:54:40 -0300

    mudei

--- Questão2.c
+++ Questão2.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 
-int main(){
-    char n[10];
+int main() {
+    int numN, num, digito;
     int soma = 0;
-    int caractere;
+
+        scanf("%d", &num);
+
+        numN = num;
+
     
-    for(int i = 0; i < 10; i++){
-    caractere = getchar();
-        if(caractere == '\n' ){
-            break;
-        }
-        n[i] = caractere;
-        soma += (caractere - '0'); 
+    if (num % 2 == 0) {
+        printf("%d eh par\n", num);
+    } else {
+        printf("%d eh impar\n", num);
     }
 
-    int n2 = atoi(n);
-
-    if(n2 % 2 == 0){
-        printf("%d eh par\n", n2);
-        printf("A soma dos algarismos de %d eh %d", n2, soma);
-    } else{
-        printf("%d eh  ímpar\n", n2);
-        printf("A soma dos algarismos de %d eh %d", n2, soma);
+    
+    while (num != 0) {
+        digito = num % 10; 
+        soma += digito;   
+        num /= 10;        
     }
+
+        printf("A soma dos algarismos de %d eh %d\n", numN, soma);
 
     return 0;
 }
